420769aa18f507e7aa70a743fa171cdd4c9a0607
cindy <kinvictorwin@gmail.com>
2023-10-24 00:16:31 +0700

    gabut

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Healthy Foods</title>
+    <title>fak</title>
 </head>
 <body>
-    <h1>Healthy Foods Kelompok 2</h1>
-    <h1>halo smetiw babi</h1>
+    fak
+    fak belom
 </body>
 </html>
